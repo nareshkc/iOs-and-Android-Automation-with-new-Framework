2235fb252362441a8ed004523e9d8093fdc616ad
vpathania1 <vishal.pathania@weather.com>
2016-06-16 10:26:44 -0400

    Update Functions.java

--- TWC.Automation.FrameWork/src/test/java/twc/Automation/General/Functions.java
+++ TWC.Automation.FrameWork/src/test/java/twc/Automation/General/Functions.java
@@ -1,154 +1,330 @@
-package twc.Automation.SmokeTestCases;
+package twc.Automation.General;
 
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.testng.Assert;
 
 import twc.Automation.Driver.Drivers;
-import twc.Automation.General.cxtgFunctions;
-import twc.Automation.General.loginModule;
-import twc.Automation.HandleWithApp.AppFunctions;
-import twc.Automation.HandleWithAppium.AppiumFunctions;
-import twc.Automation.HandleWithCharles.CharlesFunctions;
-import twc.Automation.General.Functions;
+import twc.Automation.ReadDataFromFile.read_excel_data;
+import twc.Automation.ReadDataFromFile.read_xml_data_into_buffer;
 
-public class somkeTestCases extends Drivers{
+public class Functions extends Drivers{
 	
+	public static void validate_API_Call_With_PubAds_Call(String excel_sheet_name) throws Exception{
+		
+		String apicall_results=null;
+		String pubadscall_results=null;
+		
+		Map<String, String> api_call_results = read_API_Call_Data(excel_sheet_name);
+		Map<String, String> pubads_call_results = read_Pub_Ad_Call_Data(excel_sheet_name);
+		//System.out.println(api_call_results);
+		//System.out.println(pubads_call_results);
+		if(api_call_results.keySet().size() == 1){
+			
+			for (String key : api_call_results.keySet()) {
+				//System.out.println("key: " + key + " value: " + api_call_results.get(key));
+				apicall_results = api_call_results.get(key).toString().replace("[", "").replace("]", "");
+				//System.out.println(apicall_results);
+			}
+			for (String pubkey : pubads_call_results.keySet()) {
+				//System.out.println("key: " + pubkey + " value: " + pubads_call_results.get(pubkey));
+				pubadscall_results = pubads_call_results.get(pubkey).toString().replace("[", "").replace("]", "");
+				//System.out.println(pubadscall_results);
+			}
+			
+			String[] pubadsresults = pubadscall_results.split(",");
+			for(int i=0;i<pubadsresults.length;i++){
+				if(apicall_results.contains(pubadsresults[i])){
+					System.out.println("Matched With "+ pubadscall_results +" :::: " + pubadsresults[i]);
+				}
+				else{
+					System.out.println("Does Not Matched With "+ pubadscall_results +" :::: " + pubadsresults[i]);
+				}
+			}
 
-	/* Pull To Refresh Test Case */
-	@Test(priority=0)
-	public void PulltoRefresh_Test_Case_Using_Charles() throws Exception{
-		System.out.println("================= Pull To Refresh Test Case Started =========================");
-		CharlesFunctions.startSessionBrowserData();
-		AppiumFunctions.LaunchApp();
-		AppFunctions.Pull_To_Refresh("Pulltorefresh");
-		CharlesFunctions.StopExportSessionXMLFile();
-		Functions.bb_call_validation("Pulltorefresh");
-		System.out.println("================= Pull To Refresh Test Case  End =========================");
+		}
+		else{
+			
+			for (String key : api_call_results.keySet()) {
+				//System.out.println("key: " + key + " value: " + api_call_results.get(key));
+				apicall_results = api_call_results.get(key).toString().replace("[", "").replace("]", "");
+				//ystem.out.println(apicall_results);
+			}
+			for (String pubkey : pubads_call_results.keySet()) {
+				//System.out.println("key: " + pubkey + " value: " + pubads_call_results.get(pubkey));
+				pubadscall_results = pubads_call_results.get(pubkey).toString().replace("[", "").replace("]", "");
+				//System.out.println(pubadscall_results);
+				
+				String[] pubadsresults = pubadscall_results.split(",");
+				//////////////////////////////////////////
+				for(int i=0;i<pubadsresults.length;i++){
+					if(!pubads_call_results.get(pubkey).equals("nl")){
+						if(api_call_results.get(pubkey).contains(pubadsresults[i])){
+							System.out.println("Matched With "+ pubads_call_results.get(pubkey) +" :::: " + pubadsresults[i]);
+						}
+						else{
+							System.out.println("Does Not Matched With "+ pubads_call_results.get(pubkey) +" :::: " + pubadsresults[i]);
+							Assert.fail("Does Not Matched With "+ pubads_call_results.get(pubkey) +" :::: " + pubadsresults[i]);
+						}
+					}
+					else{
+						System.out.println("Getting nl value for "+pubkey+" from pubads call");
+					}
+				}
+			}
+		}
 	}
-	/* Clean Launch  Test Case */
-	@Test(priority=1)
-	public void Clean_App_Launch() throws Exception{
-		System.out.println("================= Clean App Launch Test Case Started =========================");
-		AppiumFunctions.UnInstallApp();
-		CharlesFunctions.startSessionBrowserData();
-		AppiumFunctions.LaunchApp();
-		//AppFunctions.Kill_Launch_App();
-		AppFunctions.CleanLaunch_launch("General");
-		CharlesFunctions.StopExportSessionXMLFile();
-		Functions.clean_App_Launch("CleanLaunch");
-		System.out.println("================= Clean App Launch Test Case End =========================");
+	public static Map<String , String>  read_API_Call_Data(String excel_sheet_name) throws Exception{
+		DeviceStatus device_status = new DeviceStatus();
+		int Cap = device_status.Device_Status();
+		
+		Map<String , String> expected_map_results = new HashMap<String, String>();
+		ArrayList<String> expected_Values_List = new ArrayList<String>();
+		
+		String[][] exceldata = read_excel_data.exceldataread(excel_sheet_name);
+		
+		String validateValues = exceldata[11][Cap];
+		String[] validate_Values = validateValues.split(",");
+		
+		
+		read_xml_data_into_buffer xml_data_into_buffer = new read_xml_data_into_buffer();
+		String sb = xml_data_into_buffer.read_xml_file_into_buffer_string();
+		
+		String Read_API_Call_Data = sb.toString().substring(sb.toString().lastIndexOf(exceldata[2][Cap]));
+		String required_info = Read_API_Call_Data.toString().substring(Read_API_Call_Data.toString().indexOf(exceldata[3][Cap]));
+		
+		String expected_data = required_info.toString().substring(required_info.indexOf(exceldata[4][Cap])+7,required_info.indexOf(exceldata[5][Cap]));
+		String expectedValues = expected_data.toString();
+		
+		if(validate_Values.length == 1){
+		
+		if(expected_data.toString().contains(exceldata[11][Cap])){
+			
+			String expecteddata = expected_data.substring(expected_data.indexOf("[")+1,expected_data.indexOf("]")-1);
+			System.out.println("Expected Data ::"+expecteddata);
+			
+			String[] expecteddata_into_arrays = expecteddata.split("},");
+			String[] expectedValue = null;
+			for(String dataKeys:expecteddata_into_arrays)
+			{
+				
+				expectedValue =dataKeys.split(",");
+				
+				for(String ExpectedValuesKey:expectedValue)
+				{
+					if(ExpectedValuesKey.contains(exceldata[12][Cap]))
+					{
+						String replaceWith = ExpectedValuesKey.toString().replace("{", "").trim();
+						
+						String[] contentkey = replaceWith.toString().split(",");
+						String expected_key = contentkey[0].replaceAll("^\"|\"$","");
+						String[] contentvalue = expected_key.split(":");
+						String expected_results =contentvalue[1].replaceFirst("^\"|\"$","");
+						expected_Values_List.add(expected_results);
+						if(expected_key.contains(""))
+						{
+							Assert.assertNotNull(expected_key);
+						}
+					}
+				}
+			}
+		}
+		expected_map_results.put(exceldata[12][Cap], expected_Values_List.toString());
+		}
+		else{
+			
+			String validateSecondValues = exceldata[12][Cap];
+			String[] validate_Second_Values = validateSecondValues.split(",");
+			List<String> fgeo_res = new ArrayList<String>();
+			List<String> faud_res = new ArrayList<String>();
+			
+			JSONParser parser = new JSONParser();
+			Object obj = parser.parse(expectedValues);
+			JSONObject jsonObject = (JSONObject) obj;
+			
+			JSONArray fgeoval = (JSONArray) jsonObject.get(validate_Values[0]);
+			for(int i=0;i< fgeoval.size();i++){
+				
+				JSONObject filter = (JSONObject) fgeoval.get(i);
+				if(filter.containsKey(validate_Second_Values[0])){
+					fgeo_res.add(filter.get(validate_Second_Values[0]).toString());
+				}
+			}
+			
+			JSONArray faudval = (JSONArray) jsonObject.get(validate_Values[1]);
+			for(int i=0;i< faudval.size();i++){
+				
+				JSONObject filter = (JSONObject) faudval.get(i);
+				if(filter.containsKey(validate_Second_Values[1])){
+					faud_res.add(filter.get(validate_Second_Values[1]).toString());
+				}
+			}
+			
+			expected_map_results.put("fgeo", fgeo_res.toString());
+			expected_map_results.put("faud", faud_res.toString());
+		}
+		return expected_map_results;
 	}
-	/* CXTG  Values Test Case */
-	@Test(priority=4)
-	public void Verify_CXTG_Values() throws Exception{
-		System.out.println("================= CXTG Values Test Case Started =========================");
-		AppiumFunctions.UnInstallApp();
-		CharlesFunctions.startSessionBrowserData();
-		AppiumFunctions.LaunchApp();
-		loginModule.login();
-		AppFunctions.Kill_Launch_App();
-		cxtgFunctions.verifySavedAddressList();
-		CharlesFunctions.StopExportSessionXMLFile();
-		cxtgFunctions.validateCXTGResultsFromPubadAndTriggerCalls("WFXTrigger");
-		System.out.println("================= CXTG Values Test Case End =========================");
+	
+	public static Map<String , String> read_Pub_Ad_Call_Data(String excel_sheet_name) throws Exception{
+		
+		DeviceStatus device_status = new DeviceStatus();
+		int Cap = device_status.Device_Status();
+		
+		Map<String , String> expected_results = new HashMap<String, String>();
+		
+		String[][] exceldata = read_excel_data.exceldataread(excel_sheet_name);
+		
+		String validateValues = exceldata[16][Cap];
+		String[] validate_Values = validateValues.split(",");
+		
+		read_xml_data_into_buffer xml_data_into_buffer = new read_xml_data_into_buffer();
+		String sb = xml_data_into_buffer.read_xml_file_into_buffer_string();
+		
+		String Read_API_Call_Data = sb.toString().substring(sb.toString().lastIndexOf(exceldata[17][Cap]));
+		String required_info = Read_API_Call_Data.toString().substring(Read_API_Call_Data.toString().indexOf(exceldata[7][Cap]));
+		
+		required_info= required_info.toString().replaceAll(exceldata[8][Cap], "=");
+		required_info= required_info.toString().replaceAll(exceldata[9][Cap], "&");
+		required_info= required_info.toString().replaceAll(exceldata[10][Cap], ",");
+		
+		required_info = required_info.substring(required_info.indexOf(exceldata[14][Cap]),required_info.indexOf(exceldata[15][Cap]));
+		
+		
+		String pubad_cust_params_data = required_info.toString();
+		
+		String[] pubadvalue = pubad_cust_params_data.split(exceldata[13][Cap]);
+		
+			for(String pubadkey:pubadvalue){
+				
+				String[] key = pubadkey.split("=");
+				
+				for(int i=0;i<validate_Values.length;i++){	
+					
+					if(key[0].equals(validate_Values[i])){
+						expected_results.put(validate_Values[i], key[1].toString());
+					}
+				}
+			}
+		return expected_results;
+	}
+	
+	public static void clean_App_Launch(String excel_sheet_name) throws Exception{
+		DeviceStatus device_status = new DeviceStatus();
+		int Cap = device_status.Device_Status();
+		
+		
+		String[][] exceldata = read_excel_data.exceldataread(excel_sheet_name);
+		read_xml_data_into_buffer xml_data_into_buffer = new read_xml_data_into_buffer();
+		String sb = xml_data_into_buffer.read_xml_file_into_buffer_string();
+		
+		String feedVal=exceldata[3][Cap].toString().trim();
+
+		System.out.println("Feeds Val are :"+feedVal.trim());
+
+		int feedcount=Integer.parseInt(feedVal);
+		
+		for(int Feed=0;Feed<=feedcount;Feed++){
+			
+			String pubadcal;
+
+			if(Feed==0){
+			pubadcal = sb.toString().substring(sb.toString().lastIndexOf(exceldata[1][Cap]));
+
+				if(pubadcal.toString().contains(exceldata[1][Cap])){
+					System.out.println("BB Ad call is pressent");
+				}else{
+					System.out.println("BB Ad call not presented");
+					Assert.fail("BB Ad call not presented");
+				}
+
+			}
+			else
+			{
+				String feedcall = exceldata[2][Cap]+Feed;
+				pubadcal = sb.toString().substring(sb.toString().lastIndexOf(exceldata[2][Cap]+Feed));
+				if(pubadcal.toString().contains(feedcall)){
+					System.out.println("Feed_"+Feed +" Ad call is pressent");
+				}else{
+					System.out.println("Feed_"+Feed +" Ad call is not pressent");
+					Assert.fail();
+				}
+
+			}
+		}
+	}
+	
+	public static void bb_call_validation(String excel_sheet_name) throws Exception{
+		
+		DeviceStatus device_status = new DeviceStatus();
+		int Cap = device_status.Device_Status();
+		
+		String[][] exceldata = read_excel_data.exceldataread(excel_sheet_name);
+		read_xml_data_into_buffer xml_data_into_buffer = new read_xml_data_into_buffer();
+		String sb = xml_data_into_buffer.read_xml_file_into_buffer_string();
+		
+		String Read_API_Call_Data = sb.toString().substring(sb.toString().lastIndexOf(exceldata[17][Cap]));
+		String required_info = Read_API_Call_Data.toString().substring(Read_API_Call_Data.toString().indexOf(exceldata[17][Cap]));
+		
+		String expected_data = required_info.toString().substring(required_info.indexOf(exceldata[14][Cap]),required_info.indexOf(exceldata[15][Cap]));
+		String expectedValues = expected_data.toString();
+		
+		System.out.println("BB Call Value is : "+expectedValues);
+		if(expectedValues.contains(exceldata[17][Cap])){
+			System.out.println("BB Call generated");
+		}
+		else{
+			System.out.println("BB Call not generated");
+			Assert.fail("BB Call not generated ");
+		}
+	}
+	
+	@SuppressWarnings("unchecked")
+	public static void thirdParty_beacons_validation(String excel_sheet_name) throws Exception{
+		DeviceStatus device_status = new DeviceStatus();
+		int Cap = device_status.Device_Status();
+		
+		Thread.sleep(4000);
+		String[][] exceldata = read_excel_data.exceldataread(excel_sheet_name);
+		
+		read_xml_data_into_buffer xml_data_into_buffer = new read_xml_data_into_buffer();
+		String sb = xml_data_into_buffer.read_xml_file_into_buffer_string();
+		
+		String Read_API_Call_Data = sb.toString().substring(sb.toString().lastIndexOf(exceldata[2][Cap]));
+		String required_info = Read_API_Call_Data.toString().substring(Read_API_Call_Data.toString().indexOf(exceldata[2][Cap]));
+		String expected_data = required_info.toString().substring(required_info.indexOf(exceldata[2][Cap]),required_info.indexOf(exceldata[3][Cap]));
+		String expectedValues = expected_data.toString();
+		
+		@SuppressWarnings("rawtypes")
+		Map map = new HashMap();
+		String[] keypairs = expectedValues.split(exceldata[4][Cap]);
+		
+		for (String keyvalue : keypairs)
+		{
+		    String[] key_value = keyvalue.split(exceldata[5][Cap]);
+		    map.put(key_value[0], key_value[1]);
+		}
+		
+		if(!empty(map.get(exceldata[6][Cap])) && !empty(map.get(exceldata[7][Cap])) && !empty(map.get(exceldata[8][Cap]))){
+			System.out.println(exceldata[6][Cap]+" Value is "+map.get(exceldata[6][Cap]));
+			System.out.println(exceldata[7][Cap]+" Value is "+map.get(exceldata[7][Cap]));
+			System.out.println(exceldata[8][Cap]+" Value is "+map.get(exceldata[8][Cap]));
+		}
+		else{
+			System.out.println(exceldata[1][Cap] +" not available.");
+			Assert.fail(exceldata[1][Cap] +" not available.");
+		}
 		
 	}
-	/* Factual Call Test Case */
-	@Test(priority=2)
-	public void Factual_Test_Case_Using_Charles() throws Exception{
-		System.out.println("================= Factual Call Test Case Started =========================");
-//		CharlesFunctions.openCharlesBrowser();
-//		AppiumFunctions.LaunchApp();
-//		CharlesFunctions.StopExportSessionXMLFile();
-		Functions.validate_API_Call_With_PubAds_Call("LocationWFX");
-		System.out.println("================= Factual Call Test Case End =========================");
-	}
-	/* Lotame Call Test Case */
-	@Test(priority=3)
-	public void Lotame_Test_Case_Using_Charles() throws Exception{
-		System.out.println("================= Lotame Call Test Case Started =========================");
-//		CharlesFunctions.openCharlesBrowser();
-//		AppiumFunctions.LaunchApp();
-//		CharlesFunctions.StopExportSessionXMLFile();
-		Functions.validate_API_Call_With_PubAds_Call("Lotame");
-		System.out.println("================= Lotame Call Test Case End =========================");
-	}
-	/* Hourly Module Extended Page Ad Test Case */
-	@Test(priority=5)
-	public void Verify_Ad_Present_On_HourlyExtended_page() throws Exception {
-		System.out.println("================= Hourly Module Test Case Started =========================");
-		//AppiumFunctions.LaunchApp();
-		AppFunctions.verify_adpresent_onextended_page("Hourly");
-		System.out.println("================= Hourly Module Test Case End =========================");
-	}
-	/* Daily Module Extended Page Ad Test Case */
-	@Test(priority=6)
-	public void Verify_Ad_Present_On_DailyExtended_page() throws Exception {
-		System.out.println("================= Daily Module Test Case Started =========================");
-		//AppiumFunctions.LaunchApp();
-		AppFunctions.verify_adpresent_onextended_page("Daily");
-		System.out.println("================= Daily Module Test Case End =========================");
-	}
-	/* RADAR And MAPS Module Extended Page Ad Test Case */
-	@Test(priority=7)
-	public void Verify_Ad_Present_On_MapsExtended_page() throws Exception {
-		System.out.println("================= MAP Module Test Case Started =========================");
-		//AppiumFunctions.LaunchApp();
-		AppFunctions.verify_adpresent_onextended_page("Map");
-		System.out.println("================= MAP Module Test Case End =========================");
-	}
-	/* News Module Extended Page Ad Test Case */
-	@Test(priority=8)
-	public void Verify_Ad_Present_On_NewsExtended_page() throws Exception {
-		System.out.println("================= NEWS Module Test Case Started =========================");
-		//AppiumFunctions.LaunchApp();
-		AppFunctions.verify_adpresent_onextended_page("News");
-		System.out.println("================= NEWS Module Test Case End =========================");
+	private static boolean empty(Object object) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 
-	/* Test Mode BB Call Test Case */
-	@Test(priority=9)
-	public void Verify_TestMode_BBCall() throws Exception{
-		System.out.println("================= Verify BB Call In Test Mode Test Case Started =========================");
-		CharlesFunctions.startSessionBrowserData();
-		AppiumFunctions.LaunchApp();
-		loginModule.login();
-		AppFunctions.Change_to_Test_Mode("TestMode");
-		AppFunctions.verifyBBCallLocationFromListInTestMode("TestMode");
-		CharlesFunctions.StopExportSessionXMLFile();
-		Functions.bb_call_validation("TestMode");
-		System.out.println("================= Verify BB Call In Test Mode Test Case End =========================");
-	}
-	
-	@Test(priority=10)
-	public void Verify_ThirdParty_Beacons_Call() throws Exception{
-		System.out.println("================= ThirdpartyBecon Test Case Started =========================");
-		Functions.thirdParty_beacons_validation("ThirdpartyBecon");
-		System.out.println("================= ThirdpartyBecon Test Case End =========================");
-	}
-	
-	@Test(priority=11)
-	public void Verify_PreLoad_PubAd_Call() throws Exception{
-		System.out.println("================= PreLoad BB Call Test Case Started =========================");
-		CharlesFunctions.startSessionBrowserData();
-		AppiumFunctions.LaunchApp();
-		AppFunctions.verify_Vedio_Module_Click_On_Forecast_Video("PreRollVideo");
-		CharlesFunctions.StopExportSessionXMLFile();
-		Functions.bb_call_validation("PreRollVideo");
-		System.out.println("================= PreLoad BB Call Test Case End =========================");
-	}
-	 
-	@BeforeTest
-	public void Before_Test() throws Exception{
-		CharlesFunctions.charlesOpen();
-		AppiumFunctions.UnInstallApp();
-		CharlesFunctions.openCharlesBrowser();
-		
-	}
-	@AfterTest
-	public void After_Test() throws Exception{
-		CharlesFunctions.BrowserClosed();
-		
-	}
 }
